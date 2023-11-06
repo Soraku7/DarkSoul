c5b58bdf842ab4bcc40b2c500b9a949d8d067468
Soraku7 <2673140287@qq.com>
2023-11-06 17:41:30 +0800

    Input System Init

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     protected CharacterInputSystem _inputSystem;
+
+    private void Awake()
+    {
+        _inputSystem = GetComponent<CharacterInputSystem>();
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    
+    void Movement()
+    {
+        if (_inputSystem.playerMovement != Vector2.zero)
+        {
+            Debug.Log("Move");
+        }
+        
+    }
 }
